905413abd03dd83e6377f029fdd105df856feba8
toddkblake <todd.k.blake@gmail.com>
2017-03-03 16:49:05 -0500

    Finish CSS transitions

--- js/components/tiles.jsx
+++ js/components/tiles.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import Tile from './tile';
 
-const Tiles = ({ game }) => {
-  return (
-    <div className="tiles">
-      {
-        game.grid.tiles().map((tile, idx) => <Tile tile={ tile } key={ tile.id } />)
-      }
-    </div>
-  );
+class Tiles extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleAdd() {
+
+  }
+
+  handleRemove() {
+
+  }
+
+  render() {
+    const { game } = this.props;
+    return (
+      <div className="tiles">
+        <ReactCSSTransitionGroup
+          transitionName="tile"
+          transitionEnterTimeout={100}
+          transitionLeaveTimeout={100}
+        >
+          {
+            game.grid.tiles().map((tile, idx) => <Tile tile={ tile } key={ tile.id } />)
+          }
+        </ReactCSSTransitionGroup>
+      </div>
+    );
+  }
 }
 
 export default Tiles;
